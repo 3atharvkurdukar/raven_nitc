fa869b14c4c51221cca20bb5c291c5dfa411f49a
Pulkit Joshi <pulkitthegamejoshi@gmail.com>
2023-02-02 23:09:08 +0530

    Added Gesture to event_card

--- lib/components/event_card.dart
+++ lib/components/event_card.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:raven_nitc/pages/event_description.dart';
 
 class EventCard extends StatelessWidget {
-  const EventCard({super.key, required this.title, this.imageUrl});
+  const EventCard(
+      {super.key, required this.title, required this.id, this.imageUrl});
 
   final String title;
+  final String id;
   final String? imageUrl;
 
   @override
@@ -25,11 +28,44 @@ class EventCard extends StatelessWidget {
         ),
       );
     }
-    return Card(
-      semanticContainer: true,
-      clipBehavior: Clip.antiAliasWithSaveLayer,
-      margin: EdgeInsets.symmetric(horizontal: 8),
-      child: cardBody,
+
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => EventDescription(id: id)),
+        );
+      },
+      child: Card(
+        semanticContainer: true,
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        margin: EdgeInsets.symmetric(horizontal: 8),
+        child: cardBody,
+      ),
     );
   }
 }
+
+
+
+//  child: GestureDetector(
+//                 onTap: () {
+//                   Navigator.push(
+//                     context,
+//                     MaterialPageRoute(builder: (context) => SignUp()),
+//                   );
+//                 },
+//                 child: SizedBox(
+//                     width: 150.0,
+//                       child: Text(
+//                         'Sign Up',
+//                         style: GoogleFonts.poppins(
+//                           fontSize: 26,
+//                           color: const Color(0xff202020),
+//                           fontWeight: FontWeight.w600,
+//                           height: 0.9615384615384616,
+//                         ),
+//                         textAlign: TextAlign.center,
+//                       ),
+//                 ),
+//               )
